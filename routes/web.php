3e3c0afe3502e073e55533b667e98cb48fbaba69
DedeSnrwn13 <sunarwandede13@gmail.com>
2024-12-21 09:45:14 +0700

    feat: preparing the route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\FrontController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::name('front.')->group(function () {
+    Route::get('/', [FrontController::class, 'index'])->name('index');
+
+    Route::get('/details/{article_news:slug}', [FrontController::class, 'details'])->name('details');
+
+    Route::get('category/{category:clug}', [FrontController::class, 'category']);
+
+    Route::get('/author/{author:slug}', [FrontController::class, 'author'])->name('author');
+
+    Route::get('/search', [FrontController::class, 'search'])->name('search');
 });
